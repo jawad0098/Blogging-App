30f441d48c3f90f0cb777195692f3456a7bf8725
jawad0098 <hassanjawad858@gmail.com>
2024-03-05 10:38:21 +0500

    Create createpost.js

---
+++ src/assets/js/createpost.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup} from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPtlthMlXzblwOmN2Hg34KO3UKgEFARIY",
+  authDomain: "blogging-app-jawad.firebaseapp.com",
+  projectId: "blogging-app-jawad",
+  storageBucket: "blogging-app-jawad.appspot.com",
+  messagingSenderId: "299034744596",
+  appId: "1:299034744596:web:be0ba2d6918ff30997d401",
+  measurementId: "G-1LCN3MQRXM"
+};
